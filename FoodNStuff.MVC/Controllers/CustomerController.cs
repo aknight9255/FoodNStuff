66f6cb2cbbd8204a2012cb24a2ca02528145587e
aknight9255 <amanda.knight55@gmail.com>
2019-06-25 14:05:51 -0400

    Added links on home page

--- FoodNStuff.MVC/Controllers/CustomerController.cs
+++ FoodNStuff.MVC/Controllers/CustomerController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,7 +15,7 @@ namespace FoodNStuff.MVC.Controllers
         // GET: Customer
         public ActionResult Index()
         {
-            return View(_db.Customers.ToList());
+            return View(_db.Customers.ToList());//displays items in this database on the page which is formated in view 
         }
         //this takes you to the page.
         //Get Customer/Create
@@ -34,6 +35,34 @@ namespace FoodNStuff.MVC.Controllers
                 return RedirectToAction("Index"); //once the changes are saved this is where it will send you. 
             }
             return View(customer); //what happens if you oont enter the correct information. gives back jank form. 
+        }
+
+        //GET: Customer/edit/{id}
+        public ActionResult Edit (int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if(customer ==null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+        //Post: Customer/edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
         }
     }
 }
