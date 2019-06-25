877766b7033c1380fac1907d769062db22b51ec4
aknight9255 <amanda.knight55@gmail.com>
2019-06-25 15:36:40 -0400

    Transaction Create view

--- FoodNStuff.MVC/Controllers/TransactionController.cs
+++ FoodNStuff.MVC/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodNStuff.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,35 @@ namespace FoodNStuff.MVC.Controllers
 {
     public class TransactionController : Controller
     {
+        private ApplicationDbContext _db = new ApplicationDbContext();
         // GET: Transaction
         public ActionResult Index()
         {
+            var transactionList = _db.Transactions.OrderBy(t => t.Customer.LastName).ThenBy(t => t.Customer.FirstName).ToList();
+            return View(transactionList);
+        }
+        
+        //Get: Transaction/create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(_db.Customers.ToList(),"CustomerID", "FullName");
+            ViewBag.ProductID = new SelectList(_db.Products.ToList(), "ProductID", "ProductName");
             return View();
         }
+        //POST: Transaction/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Transactions.Add(transaction);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(transaction);
+
+        }
+
     }
 }
